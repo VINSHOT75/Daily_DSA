fa82a768703a796a75f30c5f9c9ddd379a093651
Vinayak Singh Bassan <vinayaksingh7.5.00@gmail.com>
2023-03-28 00:33:58 +0530

    Pattern problem 2

---
+++ Pattern2.js
@@ -0,0 +1,20 @@
+print following pattern
+//      *
+//      **
+//      ***
+//      ****
+//      *****
+
+
+n = 5
+let i = 1
+
+while (i <= n) {
+    let j = 1
+    while (j <= i) {
+        process.stdout.write("*")
+        j = j +1
+    }
+    i = i + 1
+    console.log()
+}
